f0eabc8010ad1fad891e29a8fc2622640a6410f2
Fatima Ali <fatimaaliforwork@gmail.com>
2024-10-03 14:01:54 +0500

    updates tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -14,6 +15,11 @@ const config: Config = {
           100: "#EBECFF",
           400: "#AAADF4",
         },
+        grey: { DEFAULT: "#B1B1B1", 100: "#EBEBEB" },
+
+        green: { DEFAULT: "#5FB537", 100: "#D9F2CD" },
+
+        red: { DEFAULT: "#E65151", 100: "#FFDFDF" },
       },
       fontFamily: {
         crimson: ["var(--font-crimson-pro)", "serif"], // Use Crimson Pro
