679fa595ca8afa8af18e4ae1ecac09020353120b
Fatima Ali <fatimaaliforwork@gmail.com>
2024-10-03 17:20:43 +0500

    integrates shadcn Ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,45 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        purple: {
-          DEFAULT: "#6A6FD5",
-          100: "#EBECFF",
-          400: "#AAADF4",
-        },
-
-        grey: { DEFAULT: "#B1B1B1", 100: "#EBEBEB" },
-
-        green: { DEFAULT: "#5FB537", 100: "#D9F2CD" },
-
-        red: { DEFAULT: "#E65151", 100: "#FFDFDF" },
-      },
-      fontFamily: {
-        crimson: ["var(--font-crimson-pro)", "serif"], // Use Crimson Pro
-        mukta: ["var(--font-mukta)", "sans-serif"], // Use Mukta
-      },
-    },
+  	extend: {
+  		colors: {
+  			purple: {
+  				'100': '#EBECFF',
+  				'400': '#AAADF4',
+  				DEFAULT: '#6A6FD5'
+  			},
+  			grey: {
+  				'100': '#EBEBEB',
+  				DEFAULT: '#B1B1B1'
+  			},
+  			green: {
+  				'100': '#D9F2CD',
+  				DEFAULT: '#5FB537'
+  			},
+  			red: {
+  				'100': '#FFDFDF',
+  				DEFAULT: '#E65151'
+  			}
+  		},
+  		fontFamily: {
+  			crimson: ["var(--font-crimson-pro)", "serif"],
+  			mukta: ["var(--font-mukta)", "sans-serif"]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
