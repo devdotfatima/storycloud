9bb66068ae0ae52cfdbd581cb4a68320211d5dd1
Fatima Ali <fatimaaliforwork@gmail.com>
2024-10-24 15:32:56 +0500

    updates functionality of reversing the route when modal is closed by clicking on overlay

--- src/app/(main)/@modal/(.)story/[storyId]/page.tsx
+++ src/app/(main)/@modal/(.)story/[storyId]/page.tsx
@@ -3,6 +3,7 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogOverlay,
   DialogTitle,
 } from "@/shared/components/ui/dialog";
 import Image from "next/image";
@@ -22,26 +23,28 @@ const Story = ({ params }: StoryDetailsProps) => {
   console.log({ storyId });
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="bg-transparent w-full  h-[100svh] sm:h-[90svh]  lg:overflow-hidden sm:max-w-[600px] lg:max-h-[600px]  pt-[20px] lg:pr-10 border-0 outline-none rounded-2xl ">
-        <DialogClose
-          onClick={handleOnClose}
-          className="absolute z-50 p-0 rounded-full cursor-pointer top-2 right-4 outline-none  w-fit lg:top-5 lg:-right-0 bg-white"
-        >
-          <Image
-            src={ClosePurpleIcon}
-            alt="Close modal"
-            className="w-6 h-6"
-            width={24}
-            height={24}
-          />
-        </DialogClose>
-        <div className="flex flex-col items-center w-full h-full  py-6  px-5 sm:px-10 mx-auto overflow-hidden bg-white shadow-md rounded-t-2xl sm:rounded-2xl gap-5 lg:gap-7 lg:py-10 justify-between  ">
-          <DialogTitle className="text-purple ">
-            your story has been sent!
-          </DialogTitle>
-        </div>
-      </DialogContent>
+    <Dialog defaultOpen>
+      <DialogOverlay onClick={handleOnClose}>
+        <DialogContent className="bg-transparent w-full  h-[100svh] sm:h-[90svh]  lg:overflow-hidden sm:max-w-[600px] lg:max-h-[600px]  pt-[20px] lg:pr-10 border-0 outline-none rounded-2xl ">
+          <DialogClose
+            onClick={handleOnClose}
+            className="absolute z-50 p-0 rounded-full cursor-pointer top-2 right-4 outline-none  w-fit lg:top-5 lg:-right-0 bg-white"
+          >
+            <Image
+              src={ClosePurpleIcon}
+              alt="Close modal"
+              className="w-6 h-6"
+              width={24}
+              height={24}
+            />
+          </DialogClose>
+          <div className="flex flex-col items-center w-full h-full  py-6  px-5 sm:px-10 mx-auto overflow-hidden bg-white shadow-md rounded-t-2xl sm:rounded-2xl gap-5 lg:gap-7 lg:py-10 justify-between  ">
+            <DialogTitle className="text-purple ">
+              your story has been sent!
+            </DialogTitle>
+          </div>
+        </DialogContent>
+      </DialogOverlay>
     </Dialog>
   );
 };
