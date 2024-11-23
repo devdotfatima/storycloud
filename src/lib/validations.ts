5616d5e2375847ccfc1813260fe099a8bb06a3c6
Fatima Ali <fatimaaliforwork@gmail.com>
2024-11-23 10:54:28 +0500

    adds validations for signing up and editing profile schema

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -4,19 +4,35 @@ const requiredString = z.string().trim().min(1, "Required");
 
 export const signUpSchema = z.object({
   email: requiredString.email("Invalid email address"),
-  username: requiredString.regex(
-    /^[a-zA-Z0-9_-]+$/,
-    "Only letters, numbers, - and _ allowed"
-  ),
+
+  birthday: z
+    .date()
+    .refine((value) => {
+      const birthday = new Date(value);
+      const age = new Date().getFullYear() - birthday.getFullYear();
+      return age >= 13;
+    }, "You must be 13 years or older to sign up.")
+    .transform((value) => new Date(value)),
+
   password: requiredString.min(8, "Must be at least 8 characters"),
-  birthday: requiredString.date(),
 });
 
-export type SignUpT = z.infer<typeof signUpSchema>;
+export const editProfileSchema = z.object({
+  fullName: requiredString.min(1, "Full name is required"),
+  username: requiredString
+    .regex(/^[a-zA-Z0-9_-]+$/, "Only letters, numbers, - and _ allowed")
+    .min(3, "Username must be at least 3 characters"),
+  bio: z.string().optional(),
+  profileImage: z.instanceof(File).optional(),
+});
 
 export const loginSchema = z.object({
   email: requiredString,
   password: requiredString,
 });
+
+export type EditProfileT = z.infer<typeof editProfileSchema>;
+
+export type SignUpT = z.infer<typeof signUpSchema>;
 
 export type LoginT = z.infer<typeof loginSchema>;
