005b90c3f83f92afae2451ee87f5776e950031de
Fatima Ali <fatimaaliforwork@gmail.com>
2024-11-26 15:54:02 +0500

    fixes build issues

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -6,6 +6,8 @@ export const POST = async () => {
     cookieStore.delete("access_token");
     return Response.json({ error: "Logged out successfully" }, { status: 200 });
   } catch (error) {
+    console.log(error);
+
     return Response.json({ error: "Internal server error" }, { status: 500 });
   }
 };
