93a9952c779768f7a76559907182bf1e0262b68a
Fatima Ali <fatimaaliforwork@gmail.com>
2024-11-26 15:44:53 +0500

    integrates API to sign out user from the app

--- src/app/(main)/settings/SignOut/SignOutConfirmationModal.tsx
+++ src/app/(main)/settings/SignOut/SignOutConfirmationModal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import {
   DialogContent,
   DialogTitle,
@@ -9,6 +8,17 @@ import {
 import CloseIcon from "../../../../assets/icons/close.svg";
 
 const SignOutConfirmationModal = () => {
+  const onLogOut = async () => {
+    const response = await fetch("/api/auth/logout", {
+      method: "POST",
+    });
+
+    if (response.ok) {
+      window.location.href = "/login"; // Redirect to the home page or login page
+    } else {
+      console.error("Logout failed");
+    }
+  };
   return (
     <DialogContent className="bg-transparent w-[85svw] lg:max-w-[450px] h-[260px] md:max-h-[324px] md:h-full overflow-y-auto lg:overflow-hidden border-0  pt-[20px] sm:pr-8 ">
       <DialogClose className="absolute z-50 p-1.5 rounded-full outline-none cursor-pointer top-2.5 right-3 sm:top-5 sm:right-0 bg-purple-400 w-fit">
@@ -25,12 +35,13 @@ const SignOutConfirmationModal = () => {
           sign out?
         </DialogTitle>
 
-        <Link
-          href={"/login"}
+        <button
+          onClick={onLogOut}
+          type="button"
           className="p-2 transition duration-150 ease-in text-red w-full bg-red-100 text-center hover:bg-red hover:text-white "
         >
           yes
-        </Link>
+        </button>
         <DialogClose
           type="button"
           className="p-2 transition duration-150 ease-in text-purple w-full  bg-purple-100 hover:bg-purple-400 hover:text-white"
