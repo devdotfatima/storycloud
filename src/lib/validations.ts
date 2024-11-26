115223f5e9e1a46e9be506d7fbd0caf43eb4f025
Fatima Ali <fatimaaliforwork@gmail.com>
2024-11-26 15:39:50 +0500

    adds validation for forgot password form

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
 
 const requiredString = z.string().trim().min(1, "Required");
+const passwordValidation = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+);
 
 export const signUpSchema = z.object({
   email: requiredString.email("Invalid email address"),
-  username: requiredString
-    .regex(/^[a-zA-Z0-9_-]+$/, "Only letters, numbers, - and _ allowed")
-    .min(3, "Username must be at least 3 characters"),
+  username: requiredString.min(3, "Username must be at least 3 characters"),
   birthday: z
     .date()
     .refine((value) => {
@@ -16,7 +17,19 @@ export const signUpSchema = z.object({
     }, "You must be 13 years or older to sign up.")
     .transform((value) => new Date(value)),
 
-  password: requiredString.min(8, "Must be at least 8 characters"),
+  password: requiredString
+    .min(8, "Must be at least 8 characters")
+    .regex(/[A-Z]/, {
+      message: " must contain at least one uppercase letter.",
+    })
+    .regex(/[a-z]/, {
+      message: " must contain at least one lowercase letter.",
+    })
+    .regex(/[0-9]/, { message: " must contain at least one number." })
+    .regex(/[^A-Za-z0-9]/, {
+      message: " must contain at least one special character.",
+    })
+    .min(8, "Must be at least 8 characters"),
 });
 
 export const editProfileSchema = z.object({
@@ -26,8 +39,12 @@ export const editProfileSchema = z.object({
 });
 
 export const loginSchema = z.object({
-  email: requiredString,
+  email: requiredString.email("Invalid email address"),
   password: requiredString,
+});
+
+export const forgotPasswordSchema = z.object({
+  email: requiredString.email("Invalid email address"),
 });
 
 export type EditProfileT = z.infer<typeof editProfileSchema>;
@@ -35,3 +52,5 @@ export type EditProfileT = z.infer<typeof editProfileSchema>;
 export type SignUpT = z.infer<typeof signUpSchema>;
 
 export type LoginT = z.infer<typeof loginSchema>;
+
+export type forgotPasswordT = z.infer<typeof forgotPasswordSchema>;
